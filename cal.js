1b6772366fbbc46a8770d10c6da147f6a28810fb
AiswaryaAjayakumar <aiswaryaajayakumar003@gmail.com>
2022-08-03 11:20:23 +0530

    Updated

--- cal.js
+++ cal.js
@@ -39,6 +39,6 @@ app.post('/div',(req,res)=>{
    var result=(getNum1/getNum2)
    res.json({'result':result}) 
 })
-app.listen(process.env.POST||3005,()=>{
+app.listen(process.env.PORT||3005,()=>{
     console.log("Server started at http://localhost:3005/")
 })
